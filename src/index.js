77a6f4ee9f99bf399df298518431c8ae59c7989b
Benjamin Semah <benjaminsemah@gmail.com>
2022-02-07 16:52:34 +0000

    Add cinimash logo to homepage

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import './style.css';
+import logoImage from './assets/Logo.png';
+
+document.querySelector('.logo').innerHTML = `<a href="#"><img class="logoImg" src="${logoImage}" alt="Cinimash" /></a>`;
