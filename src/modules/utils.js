e3ef2faf636b165d4d9a9547ba1ccfef4666a798
Yerimah <danielyerimah@yahoo.com>
2022-02-08 23:42:24 +0100

    fix linter error

--- src/modules/utils.js
+++ src/modules/utils.js
@@ -1,9 +1,9 @@
 // const involvementApi = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Rddf0xwSVTLNIiHbS9z7';
-// const MoviesApi = ' https://api.tvmaze.com/shows';
-// export { involvementApi, MoviesApi };
+
+const MoviesApi = ' https://api.tvmaze.com/shows';
 
 const fetchMovies = async () => {
-  const response = await fetch('https://api.tvmaze.com/shows');
+  const response = await fetch(MoviesApi);
   const allMovies = await response.json();
 
   return allMovies;
