4fe85691faf31053a28ebaec5bd5dfc366336ba1
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-02-08 16:42:48 +0200

    Created a function to display popup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import './style.css';
 import logoImage from './assets/Logo.png';
-
+import { displayData } from './modules/reservation.js';
 // import { involvementApi, MoviesApi } from './modules/utils.js';
 
 document.querySelector('.logo').innerHTML = `<a href="#"><img class="logoImg" src="${logoImage}" alt="Cinimash" /></a>`;
+window.addEventListener('DOMContentLoaded', () => {
+  displayData(2);
+});
