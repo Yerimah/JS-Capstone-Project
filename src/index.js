38f240e847f909b3c77805696bc7985217a1356c
Benjamin Semah <benjaminsemah@gmail.com>
2022-02-08 22:38:20 +0000

    Fix errors spotted by linters

--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ import openCommentsPopup from './modules/comments.js';
 
 document.querySelector('.logo').innerHTML = `<a href="#"><img class="logoImg" src="${logoImage}" alt="Cinimash" /></a>`;
 
-//openCommentsPopup(1);
+openCommentsPopup(1);
