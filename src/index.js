23ff425d1c9b79e9f41a86743535ab3d057b6e42
Benjamin Semah <benjaminsemah@gmail.com>
2022-02-08 23:16:59 +0000

    Fix errors spotted by linters

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import './style.css';
 import logoImage from './assets/Logo.png';
 import openCommentsPopup from './modules/comments.js';
 
+import { displayData } from './modules/reservation.js';
+
 document.querySelector('.logo').innerHTML = `<a href="#"><img class="logoImg" src="${logoImage}" alt="Cinimash" /></a>`;
 
 openCommentsPopup(1);
-
-import { displayData } from './modules/reservation.js';
 // import { involvementApi, MoviesApi } from './modules/utils.js';
 
 document.querySelector('.logo').innerHTML = `<a href="#"><img class="logoImg" src="${logoImage}" alt="Cinimash" /></a>`;
