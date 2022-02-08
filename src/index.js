9b694d9df7386884b8506f32aded65fbc7f188da
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-02-08 14:50:37 +0200

    correct linters

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './style.css';
 import logoImage from './assets/Logo.png';
-import { involvementApi, MoviesApi} from './modules/utils.js';
+
+// import { involvementApi, MoviesApi } from './modules/utils.js';
 
 document.querySelector('.logo').innerHTML = `<a href="#"><img class="logoImg" src="${logoImage}" alt="Cinimash" /></a>`;
