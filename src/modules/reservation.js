3a488f08606e86642dd6d67dbce38ce360d0272a
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-02-08 18:26:19 +0200

    Create reservation form

--- src/modules/reservation.js
+++ src/modules/reservation.js
@@ -1,4 +1,9 @@
-import { MoviesApi } from './utils.js';
+import { involvementApi, MoviesApi } from './utils.js';
+
+const popUp = document.createElement('div');
+popUp.className = 'pop-up';
+const footer = document.querySelector('footer');
+document.body.insertBefore(popUp, footer);
 
 // eslint-disable-next-line import/prefer-default-export
 export const displayData = async (id) => {
@@ -11,15 +16,19 @@ export const displayData = async (id) => {
   const MovieDuration = result.runtime;
   const Movierating = result.rating.average;
   const MovieCover = result.image.original;
-  const popUp = document.createElement('div');
-  popUp.className = 'pop-up';
-  const footer = document.querySelector('footer');
-  document.body.insertBefore(popUp, footer);
+
   const popUpContent = document.createElement('div');
   popUpContent.className = 'pop-content';
   popUpContent.innerHTML = `<div class= image-container><img src=${MovieCover} alt = 'cover picture'></div>
-    <h3 class='heading'>${MovieName}</h3><div class=details><div class='block'><span><strong>Language</strong>: ${MovieLanguage}</span>
+    <div class='description'><h2 class='heading'>${MovieName}</h2><div class=details><div class='block'><span><strong>Language</strong>: ${MovieLanguage}</span>
     <span><strong>Genre</strong>: ${MovieGenre[0]} | ${MovieGenre[1]} | ${MovieGenre[2]}</span></div><div class='block'><span><strong>Duration</strong>: ${MovieDuration} minutes</span>
-    <span><strong>Ratings</strong>: ${Movierating} / 10</span></div></div>`;
+    <span><strong>Ratings</strong>: ${Movierating} / 10</span></div></div><div class='reserve-form'><h3 class='heading'>Add Reservation</h3><form><input type='text' id='reserver' placeholder='Enter your name'>
+    <label for='start-date'>Start Date</label><input type='date' id='start-date' placeholder='Start-date YYYY/MM/DD'>
+    <label for='start-date'>End Date</label><input type='date' id='end-date' placeholder='End-date YYYY/MM/DD'>
+    <input type='submit' value='Reserve' id='res-btn'></form></div></div>`;
   popUp.appendChild(popUpContent);
+};
+
+export const addReservation = (id, username, startDate, endDate) => {
+
 };
