e661672a5877c916850ea243520aa11e4540df18
Benjamin Semah <benjaminsemah@gmail.com>
2022-02-09 08:52:38 +0000

    Add clickable link to download button

--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@ import './style.css';
 import logoImage from './assets/Logo.png';
 import openCommentsPopup, { postComments, getComments } from './modules/comments.js';
 
-// import { displayData } from './modules/reservation.js';
+import { 
+  involvementApi,
+  MoviesApi,
+  userNameInput,
+  userCommentInput,
+  postCommentsBtn,
+  submissionFail
+} from './modules/utils.js';
 
-// import { involvementApi, MoviesApi } from './modules/utils.js';
+// import { displayData } from './modules/reservation.js';
 
 document.querySelector('.logo').innerHTML = `<a href="#"><img class="logoImg" src="${logoImage}" alt="Cinimash" /></a>`;
 
@@ -12,6 +19,18 @@ document.querySelector('.logo').innerHTML = `<a href="#"><img class="logoImg" sr
 //   displayData(2);
 // });
 
-openCommentsPopup(1);
-postComments(101);
-getComments(77);
+
+
+postCommentsBtn.addEventListener('click', () => {
+  if (userNameInput.value != '' && userCommentInput.value !== '') {
+    postComments(100);
+    userNameInput.value = '';
+    userCommentInput.value = '';
+  } else {
+    submissionFail.innerHTML = 'Submission failed. Please try again.'
+  }
+  getComments(100);
+})
+
+openCommentsPopup(22);
+getComments(100);
