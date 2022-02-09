c640eab7cfcc3891d2c69f9c2640bf58ca304938
Benjamin Semah <benjaminsemah@gmail.com>
2022-02-09 03:40:42 +0000

    Retrieve dummy comments data from API

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import './style.css';
 import logoImage from './assets/Logo.png';
 import openCommentsPopup from './modules/comments.js';
-
+import { postComments, getComments } from './modules/comments.js';
 import { displayData } from './modules/reservation.js';
 
-document.querySelector('.logo').innerHTML = `<a href="#"><img class="logoImg" src="${logoImage}" alt="Cinimash" /></a>`;
-
-openCommentsPopup(1);
 // import { involvementApi, MoviesApi } from './modules/utils.js';
 
 document.querySelector('.logo').innerHTML = `<a href="#"><img class="logoImg" src="${logoImage}" alt="Cinimash" /></a>`;
-window.addEventListener('DOMContentLoaded', () => {
-  displayData(2);
-});
+
+// document.querySelector('.logo').innerHTML = `<a href="#"><img class="logoImg" src="${logoImage}" alt="Cinimash" /></a>`;
+// window.addEventListener('DOMContentLoaded', () => {
+//   displayData(2);
+// });
+
+openCommentsPopup(1);
+//postComments(77);
+getComments(77);
+
--- src/modules/comments.js
+++ src/modules/comments.js
@@ -1,7 +1,9 @@
 import {
   MoviesApi,
+  involvementApi,
+  detailsContainer,
   commentsContainer,
-  truncateSummary,
+  truncateSummary
 }
 from './utils.js';
 
@@ -26,33 +28,71 @@ const openCommentsPopup = async (id) => {
   });
 
   const commentsHTML = `
-    <div class="comments-wrapper">
-    <button class="close-btn">X</button>
-    <div class="top-container">
-      <div class="image-and-download">
-        <img class="tv-show-image" src="${movieImage}">
-        <button class="download-btn" type="button">Download</button>
+    
+      <button class="close-btn">X</button>
+      <div class="top-container">
+        <div class="image-and-download">
+          <img class="tv-show-image" src="${movieImage}">
+          <button class="download-btn" type="button">Download</button>
+        </div>
+        <div class="tv-show-info">
+          <h3 class="tv-show-title">${movieName}</h3>
+          <div class="tv-shows-genres">
+            <strong>Genre: </strong>${movieGenres}
+          </div>
+          <div class="lang-and-rating">
+            <p class="tv-show-language"><strong>Language: </strong>${movieLanguage}</p>
+            <p class="tv-show-rating"><strong>Rating: </strong>${movieRating}</p>
+          </div>
+          <div class="summary-div">
+            <strong>SUMMARY: </strong><br>
+            ${movieSummary}
+          </div>
+        </div>
       </div>
-      <div class="tv-show-info">
-        <h3 class="tv-show-title">${movieName}</h3>
-        <div class="tv-shows-genres">
-          <strong>Genre: </strong>${movieGenres}
-        </div>
-        <div class="lang-and-rating">
-          <p class="tv-show-language"><strong>Language: </strong>${movieLanguage}</p>
-          <p class="tv-show-rating"><strong>Rating: </strong>${movieRating}</p>
-        </div>
-        <div class="summary-div">
-          <strong>SUMMARY: </strong><br>
-          ${movieSummary}
-        </div>
-      </div>
-    </div>
-    <div class="bottom-container">
-    </div>
-    </div>
+    
   `;
-  commentsContainer.innerHTML = commentsHTML;
+  detailsContainer.innerHTML = commentsHTML;
 };
 
+export const postComments = async (id) => {
+  await fetch(`${involvementApi}/comments?item_id=${id}`, {
+    method: 'POST',
+    body: JSON.stringify({
+      "item_id": "77",
+      "username": "Somebody's Name",
+      "comment": "Somebody's comment"
+    }),
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8'
+    }, 
+  });
+  
+}
+
+export const getComments = async (id) => {
+  const response = await fetch(`${involvementApi}/comments?item_id=${id}`);
+  const commentsData = await response.json();
+  console.log(commentsData);
+  let allComments = ``;
+  commentsData.forEach(comment => {
+    allComments += 
+    `
+      <div class="single-comment">
+        <div class="comment-date">${comment.creation_date}</div>
+        <p class="name-and-message"><strong>${comment.username}: </strong>${comment.comment}</p>
+      </div>
+    `
+  });
+  commentsContainer.innerHTML = allComments;
+  console.log(allComments);
+}
+
 export { openCommentsPopup as default };
+
+// `
+// <div class="single-comment">
+//   <div class="comment-date">2022-02-09</div>
+//   <p class="name-and-message"><strong>Jane Austen: </strong>This is my nice comment. This is my nice comment. </p>
+//  </div>
+// `
