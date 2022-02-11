28c6f2956c525e5f91c5726bf3f669c19cf74dff
Benjamin Semah <benjaminsemah@gmail.com>
2022-02-11 16:07:44 +0000

    Add number of TV Shows on homepage

--- src/modules/display.js
+++ src/modules/display.js
@@ -1,4 +1,4 @@
-import { updateLikes } from './displaylikes.js';
+import { updateLikes, countHomeMovies } from './displaylikes.js';
 
 const display = (movies) => {
   movies.forEach((e, index) => {
@@ -6,27 +6,29 @@ const display = (movies) => {
     if (index < 24) {
       list.innerHTML += `
       <div class="grid-items">
-      <ul class="image-card">
-      <img class="movie-art" src="${e.image.medium}" alt="">
-      </ul>
-      <div class="subtitles">
-      <h4>${e.name}</h4>    
-      <span class="Like-container">
-      <span><i id="${e.id}" class="fa fa-heart bot"></i></span>
-      
-      <input type="number" id="${e.id}" class="counter" value="" name="">
-       
-      <span id="${e.id}">Likes</span>
-      </span>   
-      </div>
-      <div class="movie-btn">
-      <button class="common-btn" id="${e.id}">Comments</i></button>
-      <button class="reserve-btn" data-target="${e.id}">Reservation</i></button>
-       </div> 
+        <ul class="image-card">
+          <img class="movie-art" src="${e.image.medium}" alt="">
+        </ul>
+        <div class="subtitles">
+          <h4>${e.name}</h4>    
+          <span class="Like-container">
+          <span><i id="${e.id}" class="fa fa-heart bot"></i></span>
+          
+          <input type="number" id="${e.id}" class="counter" value="" name="">
+          
+          <span id="${e.id}">Likes</span>
+          </span>   
+        </div>
+        <div class="movie-btn">
+          <button class="common-btn" id="${e.id}">Comments</i></button>
+          <button class="reserve-btn" data-target="${e.id}">Reservation</i></button>
+        </div> 
       </div>
     `;
     }
   });
+
+  countHomeMovies();
 
   const cont = document.querySelectorAll('.counter');
   cont.forEach((input, index) => {
